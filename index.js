8309f30e09e1bf5b8485336a13273316d8039e08
Logan <loghar00@gmail.com>
2023-08-27 20:11:57 -0500

    Implemented basic autoclicker
    	AutoClicker
    	  -- class that defines a level of the clicker and a speed
    	  in ms that determines how quickly it activates
    	Game
    	  -- created instance of AutoClicker class and bound
    	  this
    	main()
    	  -- called setInterval using autoClicker instance using the
    	  level to increment and the speed to activate it

--- index.js
+++ index.js
@@ -4,23 +4,37 @@ class Button {
     buttonHtml =  document.getElementById("button");
 }
 
+class AutoClicker {
+    level = 0;
+    // speed is in ms
+    clickSpeed = 1000;
+}
 class Game {
     // binds this instance so this refers to class instance
     // instead of the global scope
     constructor() {
         this.buttonClick = this.buttonClick.bind(this)
+        this.autoClick = this.autoClick.bind(this)
     }
     button = new Button();
+    autoClicker = new AutoClicker();
     clickCounter = 0;
     clickCounterHtml = document.getElementById("counter");
     buttonClick() {
         this.clickCounter += this.button.level  
+        this.clickCounterHtml.textContent = this.clickCounter
+    };
+    // unsure if async is necessary due to setInterval implementation
+    async autoClick() {
+        this.clickCounter += this.autoClicker.level
         this.clickCounterHtml.textContent = this.clickCounter
     }
 }
 
 function main() {
     g = new Game()
+    // runs autoclicker
+    setInterval(g.autoClick, g.autoClicker.clickSpeed)
     g.button.buttonHtml.addEventListener("click", g.buttonClick)
 }
 
